374ca9e8654c7dbb272feed9064653d8204744a4
Shaik Nayeem <nayeemshaik3112@gmail.com>
2026-03-29 17:46:34 +0530

    updated the notes

--- reference-monolith/src/main/java/com/company/projectmanagement/entity/Project.java
+++ reference-monolith/src/main/java/com/company/projectmanagement/entity/Project.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class Project {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedVa lue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
